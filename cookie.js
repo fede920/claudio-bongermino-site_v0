edcbd5881e7a57257ee3c916996fee4b95f38563
fede920 <93974333+fede920@users.noreply.github.com>
2026-02-11 22:45:03 +0100

    Create cookie.js

---
+++ cookie.js
@@ -0,0 +1,4 @@
+if(!localStorage.getItem("cookieConsent")){
+  alert("Questo sito utilizza cookie tecnici e Google Maps. Continuando accetti la Cookie Policy.");
+  localStorage.setItem("cookieConsent","true");
+}
